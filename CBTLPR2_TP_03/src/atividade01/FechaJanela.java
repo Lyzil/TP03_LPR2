96b1462321905a30a00108e2eadeff5dbe1f4ceb
Lyzil <gu.verissimomonteiro@gmail.com>
2025-11-12 12:46:17 -0300

    Add files via upload

---
+++ CBTLPR2_TP_03/src/atividade01/FechaJanela.java
@@ -0,0 +1,13 @@
+package atividade01;
+/*
+ Nome: Luiz Gustavo Verissimo Monteiro
+ Prontuário: CB3030326
+*/
+import java.awt.event.*;
+class FechaJanela extends WindowAdapter
+{
+public void windowClosing(WindowEvent e)
+{
+System.exit(0);
+}
+}
---
+++ CBTLPR2_TP_03/src/atividade02/FechaJanela.java
@@ -0,0 +1,13 @@
+package atividade02;
+/*
+ Nome: Luiz Gustavo Verissimo Monteiro
+ Prontuário: CB3030326
+*/
+import java.awt.event.*;
+class FechaJanela extends WindowAdapter
+{
+public void windowClosing(WindowEvent e)
+{
+System.exit(0);
+}
+}
